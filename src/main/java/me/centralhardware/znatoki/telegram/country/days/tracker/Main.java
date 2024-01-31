6dbe46dfcb52982b6786d2966b61524c8057b4ee
Alexey Fedechkin <alex@centralhardware.me>
2024-02-01 00:19:41 +0100

    remove java files

--- src/main/java/me/centralhardware/znatoki/telegram/country/days/tracker/Main.java
+++
@@ -1,9 +0,0 @@
-package me.centralhardware.znatoki.telegram.country.days.tracker;
-
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
-public class Main {
-    public static void main(String[] args) throws TelegramApiException {
-        new CountryDaysTrackerBot();
-    }
-}
