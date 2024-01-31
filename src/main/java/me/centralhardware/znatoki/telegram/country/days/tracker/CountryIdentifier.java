ede7489a3121f8d26a7a07b9947747e1628ba5b3
Alexey Fedechkin <alex@centralhardware.me>
2024-01-31 17:37:40 +0100

    fix build

--- src/main/java/me/centralhardware/znatoki/telegram/country/days/tracker/CountryIdentifier.java
+++ src/main/java/me/centralhardware/znatoki/telegram/country/days/tracker/CountryIdentifier.java
@@ -1,12 +1,9 @@
 package me.centralhardware.znatoki.telegram.country.days.tracker;
 
-import uk.recurse.geocoding.reverse.ReverseGeocoder;
-
 import java.util.Locale;
 
 public class CountryIdentifier {
 
-    private static final ReverseGeocoder geocoder = new ReverseGeocoder();
     public static String identify(String cc){
         return Locale.of("", cc).getDisplayCountry();
     }
--- src/main/java/me/centralhardware/znatoki/telegram/country/days/tracker/TimezoneIdentifier.java
+++ src/main/java/me/centralhardware/znatoki/telegram/country/days/tracker/TimezoneIdentifier.java
@@ -1,13 +1,9 @@
 package me.centralhardware.znatoki.telegram.country.days.tracker;
 
-import net.iakovlev.timeshape.TimeZoneEngine;
-
 import java.time.ZoneId;
 import java.util.TimeZone;
 
 public class TimezoneIdentifier {
-
-    private final static TimeZoneEngine engine = TimeZoneEngine.initialize();
 
     public static ZoneId identify(String ts) {
         return TimeZone.getTimeZone(ts).toZoneId();
