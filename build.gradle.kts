4a2a5e81841efe6841e687394987b4d266fd7b8b
Alexey Fedechkin <alex@centralhardware.me>
2024-02-01 00:42:21 +0100

    fix main class

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ tasks {
         dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources")) // We need this for Gradle optimization to work
         archiveClassifier.set("standalone") // Naming the jar
         duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-        manifest { attributes(mapOf("Main-Class" to "MainKt")) } // Provided we set it up in the application plugin configuration
+        manifest { attributes(mapOf("Main-Class" to "me.centralhardware.znatoki.MainKt")) } // Provided we set it up in the application plugin configuration
         val sourcesMain = sourceSets.main.get()
         val contents = configurations.runtimeClasspath.get()
             .map { if (it.isDirectory) it else zipTree(it) } +
