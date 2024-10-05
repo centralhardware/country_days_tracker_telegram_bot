1456c4f2cbed10c84405bb73b77a8ce53d0459ea
Fedechkin Aleksei <alex@centralhardware.me>
2024-10-05 18:24:27 +0200

    fix clickhouse

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ val ktorVersion = "2.3.12";
 
 dependencies {
     implementation("dev.inmo:tgbotapi:18.2.1")
+    implementation("org.apache.httpcomponents.client5:httpclient5:5.3.1")
     implementation("com.clickhouse:clickhouse-jdbc:0.6.5")
     implementation("org.lz4:lz4-java:1.8.0")
     implementation("org.slf4j:slf4j-simple:$slf4jVersion")
