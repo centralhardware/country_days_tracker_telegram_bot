6d7985e91e17ac9a7710eae0ece905d2ed423bb8
Fedechkin Aleksei <alex@centralhardware.me>
2024-10-09 21:39:12 +0200

    bump commons

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ val ktorVersion = "2.3.12";
 
 dependencies {
     implementation("dev.inmo:tgbotapi:18.2.1")
-    implementation("com.github.centralhardware:telegram-bot-commons:efefa401e3")
+    implementation("com.github.centralhardware:telegram-bot-commons:9e2fdcf466")
     implementation("org.apache.httpcomponents.client5:httpclient5:5.3.1")
     implementation("com.clickhouse:clickhouse-jdbc:0.6.5")
     implementation("org.lz4:lz4-java:1.8.0")
