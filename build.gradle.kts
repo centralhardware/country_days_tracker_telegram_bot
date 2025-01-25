a9cb93444a2261c8168ed258244b597b9bb5d330
Fedechkin Aleksei <alex@centralhardware.me>
2025-01-25 16:07:44 +0100

    bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ val clickhouseVersion = "0.8.0"
 
 dependencies {
     implementation("dev.inmo:tgbotapi:23.1.0")
-    implementation("com.github.centralhardware:telegram-bot-commons:2b13fe6b9f")
+    implementation("com.github.centralhardware:telegram-bot-commons:8e638b8b19")
     implementation("com.github.centralhardware:ktgbotapi-restrict-access-middleware:b0e1f48293")
     implementation("com.clickhouse:clickhouse-jdbc:$clickhouseVersion")
     implementation("com.clickhouse:clickhouse-http-client:$clickhouseVersion")
