41caedbf8338929074cb10281109901e3c1fe67d
Fedechkin Aleksei <alex@centralhardware.me>
2025-02-17 07:11:07 +0100

    bump ktgbotapi

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,11 +14,11 @@ repositories {
     maven("https://jitpack.io")
 }
 
-val ktorVersion = "3.1.0";
+val ktorVersion = "3.0.3";
 val clickhouseVersion = "0.8.0"
 
 dependencies {
-    implementation("dev.inmo:tgbotapi:23.1.2")
+    implementation("dev.inmo:tgbotapi:23.2.0")
     implementation("com.github.centralhardware:ktgbotapi-commons:f0db12f60b")
     implementation("com.github.centralhardware:ktgbotapi-restrict-access-middleware:b0e1f48293")
     implementation("com.clickhouse:clickhouse-jdbc:$clickhouseVersion")
