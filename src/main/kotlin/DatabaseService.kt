582769e53469ce2f118d0c8738d848d460fec027
Fedechkin Aleksei <alex@centralhardware.me>
2025-04-20 13:10:17 +0200

    Refactor database handling and add new trip statistics command

---
+++ src/main/kotlin/DatabaseService.kt
@@ -0,0 +1,197 @@
+
+import com.clickhouse.jdbc.ClickHouseDataSource
+import dev.inmo.kslog.common.KSLog
+import dev.inmo.kslog.common.info
+import kotliquery.queryOf
+import kotliquery.sessionOf
+import java.sql.SQLException
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import javax.sql.DataSource
+
+class DatabaseService {
+    companion object {
+        val dataSource: DataSource = try {
+            ClickHouseDataSource(System.getenv("CLICKHOUSE_URL"))
+        } catch (e: SQLException) {
+            throw RuntimeException(e)
+        }
+    }
+
+    fun save(latitude: Float, longitude: Float, ts: ZoneId, country: String, userId: Long) {
+        KSLog.info("lat: $latitude, lon: $longitude, ts: $ts, cc: $country")
+
+        sessionOf(dataSource)
+            .execute(
+                queryOf(
+                    """
+                    INSERT INTO country_days_tracker_bot.country_days_tracker
+                    ( date_time,
+                      user_id,
+                      latitude,
+                      longitude,
+                      country,
+                      tzname
+                    )
+                    VALUES (
+                      :date_time,
+                      :user_id,
+                      :latitude,
+                      :longitude,
+                      :country,
+                      :tzname)
+                    """,
+                    mapOf(
+                        "date_time" to ZonedDateTime.now().withZoneSameInstant(ts).toLocalDateTime(),
+                        "user_id" to userId,
+                        "latitude" to latitude,
+                        "longitude" to longitude,
+                        "country" to country,
+                        "tzname" to ts.id,
+                    ),
+                )
+            )
+    }
+
+    fun getCountryStats(userId: Long): List<Pair<String, Int>> {
+        return sessionOf(dataSource)
+            .run(
+                queryOf(
+                    """
+                    SELECT country, count(*) as count_of_days
+                    FROM (
+                      SELECT DISTINCT lower(country) as country,toStartOfDay(date_time)
+                      FROM country_days_tracker_bot.country_days_tracker
+                      WHERE user_id = :user_id
+                    )
+                    GROUP BY country
+                    ORDER BY count(*) DESC
+                    """,
+                    mapOf("user_id" to userId),
+                )
+                .map { row ->
+                    Pair(row.string("country"), row.int("count_of_days"))
+                }
+                .asList
+            )
+    }
+
+    fun getTrips(userId: Long, countryName: String): List<Triple<String, Pair<LocalDate, LocalDate>, Int>> {
+        return sessionOf(dataSource)
+            .run(
+                queryOf(
+                    """
+                    WITH
+                    -- Remove time, keep only date
+                    data AS (
+                        SELECT
+                            user_id,
+                            toDate(date_time) AS day,
+                            country
+                        FROM country_days_tracker_bot.country_days_tracker
+                        WHERE user_id = :user_id
+                        AND lower(country) = lower(:country_name)
+                        GROUP BY
+                            user_id, day, country
+                    ),
+
+                    -- Define session boundaries by continuous dates
+                    with_sessions AS (
+                        SELECT
+                            *,
+                            day - toIntervalDay(row_number() OVER (PARTITION BY user_id, country ORDER BY day)) AS session_id
+                        FROM data
+                    ),
+
+                    -- Group by sessions, count duration
+                    sessions_grouped AS (
+                        SELECT
+                            user_id,
+                            country,
+                            min(day) AS start_day,
+                            max(day) AS end_day,
+                            count() AS days_in_country
+                        FROM with_sessions
+                        GROUP BY
+                            user_id, country, session_id
+                    )
+
+                    -- Output all sessions for the specified country
+                    SELECT
+                        country,
+                        start_day,
+                        end_day,
+                        days_in_country
+                    FROM sessions_grouped
+                    WHERE user_id = :user_id
+                    ORDER BY start_day DESC
+                    """,
+                    mapOf(
+                        "user_id" to userId,
+                        "country_name" to countryName
+                    )
+                )
+                .map { row ->
+                    Triple(
+                        row.string("country"),
+                        Pair(row.localDate("start_day"), row.localDate("end_day")),
+                        row.int("days_in_country")
+                    )
+                }
+                .asList
+            )
+    }
+
+    fun getCurrentCountryLength(): Pair<String, Int> {
+        return sessionOf(dataSource)
+            .run(queryOf(
+                """
+                WITH
+                -- Remove time, keep only date
+                data AS (
+                    SELECT
+                        user_id,
+                        toDate(date_time) AS day,
+                        country
+                    FROM country_days_tracker_bot.country_days_tracker
+                    GROUP BY
+                        user_id, day, country
+                ),
+
+                -- Define session boundaries by country change
+                with_sessions AS (
+                    SELECT
+                        *,
+                        row_number() OVER (PARTITION BY user_id ORDER BY day) -
+                        row_number() OVER (PARTITION BY user_id, country ORDER BY day) AS session_id
+                    FROM data
+                ),
+
+                -- Group by sessions, count duration
+                sessions_grouped AS (
+                    SELECT
+                        user_id,
+                        country,
+                        min(day) AS start_day,
+                        max(day) AS end_day,
+                        count() AS days_in_country
+                    FROM with_sessions
+                    GROUP BY
+                        user_id, country, session_id
+                )
+
+                -- Output only the current session (maximum by date)
+                SELECT
+                    country,
+                    days_in_country
+                FROM (
+                     SELECT *,
+                            row_number() OVER (PARTITION BY user_id ORDER BY end_day DESC) AS rn
+                     FROM sessions_grouped
+                     )
+                WHERE rn = 1 
+                """, mapOf()
+            ).map { row -> Pair(row.string("country"), row.int("days_in_country")) }.asSingle)!!
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,4 @@
 
-import com.clickhouse.jdbc.ClickHouseDataSource
 import dev.inmo.kslog.common.KSLog
 import dev.inmo.kslog.common.info
 import dev.inmo.micro_utils.common.Warning
@@ -8,6 +7,7 @@ import dev.inmo.tgbotapi.Trace
 import dev.inmo.tgbotapi.extensions.api.bot.setMyCommands
 import dev.inmo.tgbotapi.extensions.api.send.reply
 import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onCommand
+import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onCommandWithArgs
 import dev.inmo.tgbotapi.extensions.utils.extensions.raw.from
 import dev.inmo.tgbotapi.longPolling
 import dev.inmo.tgbotapi.types.BotCommand
@@ -17,23 +17,14 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import kotliquery.queryOf
-import kotliquery.sessionOf
 import me.centralhardware.telegram.EnvironmentVariableUserAccessChecker
 import me.centralhardware.telegram.restrictAccess
-import java.sql.SQLException
 import java.time.ZoneId
-import java.time.ZonedDateTime
 import java.util.*
 import java.util.concurrent.atomic.AtomicInteger
-import javax.sql.DataSource
 
-val dataSource: DataSource =
-    try {
-        ClickHouseDataSource(System.getenv("CLICKHOUSE_URL"))
-    } catch (e: SQLException) {
-        throw RuntimeException(e)
-    }
+// Database service instance
+private val dbService = DatabaseService()
 
 fun toTimeZone(ts: String): ZoneId = TimeZone.getTimeZone(ts).toZoneId()
 
@@ -63,7 +54,7 @@ suspend fun main() {
                             "Missing or invalid query parameters",
                         )
                     } else {
-                        runCatching { save(latitude, longitude, toTimeZone(timezone), country, userId) }
+                        runCatching { dbService.save(latitude, longitude, toTimeZone(timezone), toCountry(country), userId) }
                             .onFailure(::println)
                         call.respond(HttpStatusCode.OK)
                     }
@@ -72,81 +63,63 @@ suspend fun main() {
         }
         .start(wait = false)
     longPolling({ restrictAccess(EnvironmentVariableUserAccessChecker()) }) {
-            setMyCommands(BotCommand("stat", "вывести статистику"))
+            setMyCommands(
+                BotCommand("stat", "show statistics"),
+                BotCommand("trips", "show date ranges of trips to a specified country")
+            )
             onCommand("stat") {
                 val i = AtomicInteger(1)
 
                 Trace.save("checkStat", mapOf("chatId" to it.from!!.id.chatId.long.toString()))
-                val stat =
-                    sessionOf(dataSource)
-                        .run(
-                            queryOf(
-                                    """
-                                  SELECT country, count(*) as count_of_days
-                                  FROM (
-                                    SELECT DISTINCT lower(country) as country,toStartOfDay(date_time)
-                                    FROM country_days_tracker_bot.country_days_tracker
-                                    WHERE user_id = :user_id
-                                  )
-                                  GROUP BY country
-                                  ORDER BY count(*) DESC
-                """,
-                                    mapOf("user_id" to it.chat.id.chatId),
-                                )
-                                .map { row ->
-                                    Pair(row.string("country"), row.int("count_of_days"))
-                                }
-                                .asList
-                        )
+                val stat = dbService.getCountryStats(it.chat.id.chatId.long)
                 val msg = buildString {
                     append(stat.joinToString("\n") { "${i.getAndIncrement()} - ${it.first} - ${it.second}(${prettyTime(it.second)})" })
                     append("\n\n")
                     append(calculateVisitedPercentage(stat.size) + "\n")
-                    append("Current country:" + getCurrentCountryLength())
+                    val currentCountry = dbService.getCurrentCountryLength()
+                    append("Current country: ${currentCountry.first} ${currentCountry.second}")
                 }
 
                 KSLog.info(stat)
                 reply(it, msg)
+            }
+
+            onCommandWithArgs("trips") { it, args ->
+                val commandText = it.content.text
+
+                if (args.isNotEmpty()) {
+                    val countryName = args.first()
+
+                    // Get trip date ranges for the specified country
+                    val trips = dbService.getTrips(it.chat.id.chatId.long, countryName)
+
+                    if (trips.isEmpty()) {
+                        reply(it, "No trips to country '$countryName' found.")
+                    } else {
+                        // Find the longest stay
+                        val longestStay = trips.maxByOrNull { it.third } ?: trips.first()
+                        val longestStayDays = longestStay.third
+
+                        val msg = buildString {
+                            append("Trips to ${trips.first().first}:\n\n")
+                            append("Longest stay: ${longestStayDays} ${getDaysWord(longestStayDays)}\n\n")
+                            trips.forEachIndexed { index, trip ->
+                                val (_, dateRange, days) = trip
+                                val (startDate, endDate) = dateRange
+                                append("${index + 1}. ${startDate} - ${endDate} (${days} ${getDaysWord(days)})\n")
+                            }
+                        }
+                        reply(it, msg)
+                    }
+                } else {
+                    reply(it, "Please use the format: /trips /t <country name>")
+                }
             }
         }
         .second
         .join()
 }
 
-fun save(latitude: Float, longitude: Float, ts: ZoneId, country: String, userId: Long) {
-    KSLog.info("lat: $latitude, lon: $longitude, ts: $ts, cc: $country")
-
-    sessionOf(dataSource)
-        .execute(
-            queryOf(
-                """
-                              INSERT INTO country_days_tracker_bot.country_days_tracker
-                              ( date_time,
-                                user_id,
-                                latitude,
-                                longitude,
-                                country,
-                                tzname
-                              )
-                              VALUES (
-                                :date_time,
-                                :user_id,
-                                :latitude,
-                                :longitude,
-                                :country,
-                                :tzname)
-            """,
-                mapOf(
-                    "date_time" to ZonedDateTime.now().withZoneSameInstant(ts).toLocalDateTime(),
-                    "user_id" to userId,
-                    "latitude" to latitude,
-                    "longitude" to longitude,
-                    "country" to toCountry(country),
-                    "tzname" to ts.id,
-                ),
-            )
-        )
-}
 
 const val TOTAL_COUNTRIES = 193
 fun calculateVisitedPercentage(visitedCountries: Int): String {
@@ -176,55 +149,6 @@ fun prettyTime(totalDays: Int): String {
     return parts.joinToString(", ").ifEmpty { "" }
 }
 
-fun getCurrentCountryLength(): String {
-    var res = sessionOf(dataSource)
-        .run(queryOf(
-            """
-               WITH
-    -- Убираем время, оставляем только дату
-    data AS (
-        SELECT
-            user_id,
-            toDate(date_time) AS day,
-            country
-        FROM country_days_tracker
-        GROUP BY
-            user_id, day, country
-    ),
-
-    -- Определяем границы "сессий" по смене страны
-    with_sessions AS (
-        SELECT
-            *,
-            row_number() OVER (PARTITION BY user_id ORDER BY day) -
-            row_number() OVER (PARTITION BY user_id, country ORDER BY day) AS session_id
-        FROM data
-    ),
-
-    -- Группируем по сессиям, считаем продолжительность
-    sessions_grouped AS (
-        SELECT
-            user_id,
-            country,
-            min(day) AS start_day,
-            max(day) AS end_day,
-            count() AS days_in_country
-        FROM with_sessions
-        GROUP BY
-            user_id, country, session_id
-    )
-
--- Выводим только текущую сессию (максимальную по дате)
-SELECT
-    country,
-    days_in_country
-FROM (
-         SELECT *,
-                row_number() OVER (PARTITION BY user_id ORDER BY end_day DESC) AS rn
-         FROM sessions_grouped
-         )
-WHERE rn = 1 
-            """, mapOf()
-        ).map { row -> Pair(row.string("country"), row.int("days_in_country")) }.asSingle)!!
-    return "${res.first} ${res.second}"
+fun getDaysWord(days: Int): String {
+    return if (days == 1) "day" else "days"
 }
