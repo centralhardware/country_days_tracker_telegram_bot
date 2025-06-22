18ac6fd202026a6066bd26c1acf46b6dd319ae8a
Alexey Fedechkin <alex@centralhardware.me>
2025-06-23 04:12:03 +0800

    Handle missing numeric values in Lua JSON sender

--- scripts/send-to-bot.lua
+++ scripts/send-to-bot.lua
@@ -2,32 +2,38 @@ function otr_init()
 end
 
 function otr_hook(topic, _type, data)
-	-- Функция экранирования строк для JSON
-	local function escape_json(str)
-		if str == nil then return "" end
-		str = tostring(str)
-		str = str:gsub('\\', '\\\\')
-		str = str:gsub('"', '\\"')
-		str = str:gsub('\n', '\\n')
-		str = str:gsub('\r', '\\r')
-		str = str:gsub('\t', '\\t')
-		return str
-	end
+        -- Escapes string values for JSON
+        local function escape_json(str)
+                if str == nil then return "" end
+                str = tostring(str)
+                str = str:gsub('\\', '\\\\')
+                str = str:gsub('"', '\\"')
+                str = str:gsub('\n', '\\n')
+                str = str:gsub('\r', '\\r')
+                str = str:gsub('\t', '\\t')
+                return str
+        end
+
+        -- Convert numeric values to strings and fallback to 0 when nil
+        local function escape_json_number(num)
+                if num == nil then return "0" end
+                return tostring(num)
+        end
 
         local json = '{' ..
-                        '"latitude":'  .. escape_json(data['lat']) .. ',' ..
-                        '"longitude":' .. escape_json(data['lon']) .. ',' ..
-                        '"timestamp":' .. escape_json(data['tst']) .. ',' ..
+                        '"latitude":'  .. escape_json_number(data['lat']) .. ',' ..
+                        '"longitude":' .. escape_json_number(data['lon']) .. ',' ..
+                        '"timestamp":' .. escape_json_number(data['tst']) .. ',' ..
                         '"timezone":"'  .. escape_json(data['tzname']) .. '",' ..
                         '"country":"'   .. escape_json(data['cc']) .. '",' ..
-                        '"alt":'       .. escape_json(data['alt']) .. ',' ..
-                        '"batt":'      .. escape_json(data['batt']) .. ',' ..
-                        '"acc":'       .. escape_json(data['acc']) .. ',' ..
-                        '"vac":'       .. escape_json(data['vac']) .. ',' ..
+                        '"alt":'       .. escape_json_number(data['alt']) .. ',' ..
+                        '"batt":'      .. escape_json_number(data['batt']) .. ',' ..
+                        '"acc":'       .. escape_json_number(data['acc']) .. ',' ..
+                        '"vac":'       .. escape_json_number(data['vac']) .. ',' ..
                         '"conn":"'      .. escape_json(data['conn']) .. '",' ..
                         '"locality":"'  .. escape_json(data['locality']) .. '",' ..
                         '"ghash":"'     .. escape_json(data['ghash']) .. '",' ..
-                        '"p":'         .. escape_json(data['p']) .. ',' ..
+                        '"p":'         .. escape_json_number(data['p']) .. ',' ..
                         '"addr":"'      .. escape_json(data['addr']) .. '"' ..
                         '}'
 
