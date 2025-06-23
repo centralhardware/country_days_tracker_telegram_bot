0c8fe003c4a1dc4e5e5e94dd493427183000a685
Alexey Fedechkin <alex@centralhardware.me>
2025-06-23 09:50:32 +0800

    Format SQL queries and add IDE annotations

--- src/main/kotlin/DatabaseService.kt
+++ src/main/kotlin/DatabaseService.kt
@@ -37,25 +37,40 @@ class DatabaseService {
         sessionOf(dataSource).use { session ->
             session.execute(
                 queryOf(
+                    // language=SQL
                     """
-    INSERT INTO country_days_tracker_bot.country_days_tracker
-    (date_time, latitude, longitude, country, tzname, alt, batt, acc, vac, conn, locality, ghash, p, addr)
-    SELECT 
-        toDateTime(?) AS date_time,
-        toFloat32(?) AS latitude,
-        toFloat32(?) AS longitude,
-        toString(?) AS country,
-        toString(?) AS tzname,
-        toUInt16(?) AS alt,
-        toUInt8(?) AS batt,
-        toUInt8(?) AS acc,
-        toUInt8(?) AS vac,
-        toString(?) AS conn,
-        toString(?) AS locality,
-        toString(?) AS ghash,
-        toFloat64(?) AS p,
-        toString(?) AS addr
-    """,
+                        INSERT INTO country_days_tracker_bot.country_days_tracker (
+                            date_time,
+                            latitude,
+                            longitude,
+                            country,
+                            tzname,
+                            alt,
+                            batt,
+                            acc,
+                            vac,
+                            conn,
+                            locality,
+                            ghash,
+                            p,
+                            addr
+                        )
+                        SELECT
+                            toDateTime(?) AS date_time,
+                            toFloat32(?) AS latitude,
+                            toFloat32(?) AS longitude,
+                            toString(?) AS country,
+                            toString(?) AS tzname,
+                            toUInt16(?) AS alt,
+                            toUInt8(?) AS batt,
+                            toUInt8(?) AS acc,
+                            toUInt8(?) AS vac,
+                            toString(?) AS conn,
+                            toString(?) AS locality,
+                            toString(?) AS ghash,
+                            toFloat64(?) AS p,
+                            toString(?) AS addr
+                    """.trimIndent(),
                     dateTime,
                     latitude,
                     longitude,
@@ -81,15 +96,16 @@ class DatabaseService {
         return sessionOf(dataSource)
             .run(
                 queryOf(
+                    // language=SQL
                     """
-                    SELECT country, count(*) as count_of_days
-                    FROM (
-                      SELECT DISTINCT lower(country) as country,toStartOfDay(date_time)
-                      FROM country_days_tracker_bot.country_days_tracker
-                    )
-                    GROUP BY country
-                    ORDER BY count(*) DESC
-                    """,
+                        SELECT country, COUNT(*) AS count_of_days
+                        FROM (
+                            SELECT DISTINCT LOWER(country) AS country, toStartOfDay(date_time)
+                            FROM country_days_tracker_bot.country_days_tracker
+                        )
+                        GROUP BY country
+                        ORDER BY COUNT(*) DESC
+                    """.trimIndent(),
                     mapOf(),
                 )
                     .map { row ->
@@ -103,52 +119,46 @@ class DatabaseService {
         return sessionOf(dataSource)
             .run(
                 queryOf(
+                    // language=SQL
                     """
-                    WITH
-                    -- Remove time, keep only date
-                    data AS (
+                        WITH
+                            data AS (
+                                SELECT
+                                    user_id,
+                                    toDate(date_time) AS day,
+                                    country
+                                FROM country_days_tracker_bot.country_days_tracker
+                                WHERE user_id = :user_id
+                                  AND LOWER(country) = LOWER(:country_name)
+                                GROUP BY user_id, day, country
+                            ),
+                            with_sessions AS (
+                                SELECT
+                                    *,
+                                    day - toIntervalDay(
+                                        row_number() OVER (PARTITION BY user_id, country ORDER BY day)
+                                    ) AS session_id
+                                FROM data
+                            ),
+                            sessions_grouped AS (
+                                SELECT
+                                    user_id,
+                                    country,
+                                    MIN(day) AS start_day,
+                                    MAX(day) AS end_day,
+                                    COUNT() AS days_in_country
+                                FROM with_sessions
+                                GROUP BY user_id, country, session_id
+                            )
                         SELECT
-                            user_id,
-                            toDate(date_time) AS day,
-                            country
-                        FROM country_days_tracker_bot.country_days_tracker
-                        WHERE user_id = :user_id
-                        AND lower(country) = lower(:country_name)
-                        GROUP BY
-                            user_id, day, country
-                    ),
-
-                    -- Define session boundaries by continuous dates
-                    with_sessions AS (
-                        SELECT
-                            *,
-                            day - toIntervalDay(row_number() OVER (PARTITION BY user_id, country ORDER BY day)) AS session_id
-                        FROM data
-                    ),
-
-                    -- Group by sessions, count duration
-                    sessions_grouped AS (
-                        SELECT
-                            user_id,
                             country,
-                            min(day) AS start_day,
-                            max(day) AS end_day,
-                            count() AS days_in_country
-                        FROM with_sessions
-                        GROUP BY
-                            user_id, country, session_id
-                    )
-
-                    -- Output all sessions for the specified country
-                    SELECT
-                        country,
-                        start_day,
-                        end_day,
-                        days_in_country
-                    FROM sessions_grouped
-                    WHERE user_id = :user_id
-                    ORDER BY start_day DESC
-                    """,
+                            start_day,
+                            end_day,
+                            days_in_country
+                        FROM sessions_grouped
+                        WHERE user_id = :user_id
+                        ORDER BY start_day DESC
+                    """.trimIndent(),
                     mapOf(
                         "country_name" to countryName
                     )
@@ -168,46 +178,45 @@ class DatabaseService {
         return sessionOf(dataSource)
             .run(
                 queryOf(
-                """
-                WITH
-data AS (
-        SELECT
-            toDate(date_time) AS day,
-            country
-        FROM country_days_tracker_bot.country_days_tracker
-        GROUP BY day, country
-    ),
-
-    with_sessions AS (
-        SELECT
-            *,
-            row_number() OVER (ORDER BY day) -
-            row_number() OVER (PARTITION BY country ORDER BY day) AS session_id
-        FROM data
-    ),
-
-    sessions_grouped AS (
-        SELECT
-            country,
-            min(day) AS start_day,
-            max(day) AS end_day,
-            count() AS days_in_country
-        FROM with_sessions
-        GROUP BY country, session_id
-    )
-
-SELECT
-    country,
-    days_in_country
-FROM (
-    SELECT *,
-           row_number() OVER (ORDER BY end_day DESC) AS rn
-    FROM sessions_grouped
-)
-WHERE rn = 1
- 
-                """, mapOf()
-            ).map { row -> Pair(row.string("country"), row.int("days_in_country")) }.asSingle
+                    // language=SQL
+                    """
+                        WITH
+                            data AS (
+                                SELECT
+                                    toDate(date_time) AS day,
+                                    country
+                                FROM country_days_tracker_bot.country_days_tracker
+                                GROUP BY day, country
+                            ),
+                            with_sessions AS (
+                                SELECT
+                                    *,
+                                    row_number() OVER (ORDER BY day) -
+                                    row_number() OVER (PARTITION BY country ORDER BY day) AS session_id
+                                FROM data
+                            ),
+                            sessions_grouped AS (
+                                SELECT
+                                    country,
+                                    MIN(day) AS start_day,
+                                    MAX(day) AS end_day,
+                                    COUNT() AS days_in_country
+                                FROM with_sessions
+                                GROUP BY country, session_id
+                            )
+                        SELECT
+                            country,
+                            days_in_country
+                        FROM (
+                            SELECT
+                                *,
+                                row_number() OVER (ORDER BY end_day DESC) AS rn
+                            FROM sessions_grouped
+                        )
+                        WHERE rn = 1
+                    """.trimIndent(),
+                    mapOf()
+                ).map { row -> Pair(row.string("country"), row.int("days_in_country")) }.asSingle
             )!!
     }
 }
